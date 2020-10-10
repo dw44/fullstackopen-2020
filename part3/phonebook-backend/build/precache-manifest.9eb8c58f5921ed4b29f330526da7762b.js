5f030bd02582471f7231b0f19f0b835e3509097f
Tanzeel <35539906+dw44@users.noreply.github.com>
2020-10-10 16:54:09 +0500

    deployment ready

---
+++ part3/phonebook-backend/build/precache-manifest.9eb8c58f5921ed4b29f330526da7762b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2de370527f757bb13e98edc9d4a1014",
+    "url": "/index.html"
+  },
+  {
+    "revision": "739c99ce50a9544c03d4",
+    "url": "/static/css/main.a1c7cb18.chunk.css"
+  },
+  {
+    "revision": "9bd8b3b232aeef7a1642",
+    "url": "/static/js/2.cb6c12d0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.cb6c12d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "739c99ce50a9544c03d4",
+    "url": "/static/js/main.5fb54a5a.chunk.js"
+  },
+  {
+    "revision": "1be37c5dc7b94362857d",
+    "url": "/static/js/runtime-main.8f2f098c.js"
+  }
+]);
