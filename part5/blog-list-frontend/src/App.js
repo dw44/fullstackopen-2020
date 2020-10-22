39a19e66ca8c1badf0d88e1adc7b7b2cf9afb6fc
Tanzeel <35539906+dw44@users.noreply.github.com>
2020-10-23 02:48:33 +0500

    5.7 done

--- part5/blog-list-frontend/src/App.js
+++ part5/blog-list-frontend/src/App.js
@@ -24,7 +24,7 @@ const App = () => {
     blogService.getAll().then(blogs =>
       setBlogs( blogs )
     );  
-  }, []);
+  }, [blogs]);
 
   useEffect(() => {
     const loggedInUser = JSON.parse(window.localStorage.getItem('loggedInUser'));
--- part5/blog-list-frontend/src/components/Blog/Blog.js
+++ part5/blog-list-frontend/src/components/Blog/Blog.js
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import classes from './Blog.module.css';
 
-const Blog = ({ blog }) => (
-  <div className={classes.BlogDiv}>
-   {blog.title}&nbsp;&nbsp; - 
-   <span className={classes.author}>&nbsp;&nbsp;{blog.author}</span>
-  </div>
-);
+// updated for 5.7
+const Blog = ({ blog }) => {
+  const [showDetail, setShowDetail] = useState(true);
+  const toggleShowDetail = () => setShowDetail(!showDetail);
+
+  return (
+    <div className={classes.BlogDiv}>
+      {blog.title}&nbsp;&nbsp; - 
+      <span className={classes.author}>&nbsp;&nbsp;{blog.author}</span>
+      <button onClick={ toggleShowDetail } className={ classes.ShowDetailBtn }>
+        { showDetail ? 'Hide Details' : 'Show Details'}
+      </button>
+      <div className={ classes.Details } style={ showDetail ? null : { display: 'none' } }>
+        <p>Likes: { blog.likes } <button className={classes.Like}>Like</button></p>
+        <p>URL: { blog.url }</p>
+        <p>Added By: { blog.user.name }</p>
+      </div>
+
+   </div>
+  );
+}
+
+
 
 export default Blog;
